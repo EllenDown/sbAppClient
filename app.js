92d9bcf7b1e74a447f7ff3810f3d6aff3bf9d348
EllenDown <ellenldowning@gmail.com>
2017-07-27 11:35:53 -0600

    successful post commit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ $(document).ready(function() {
   $('select').material_select();
 });
 
+$('#addInput').click(sendPostReqest)
+$('#editInput').click(sendPutReqest)
+$('#deleteInput').click(sendDeleteReqest)
+
 $('#user_name').on('click', function(event){
   event.preventDefault()
 });
@@ -84,33 +88,15 @@ $('#deleteItem').on('click', function(event) {
   );
 }
 }
-// $('.addButton').click(function() {
-//   event.preventDefault();
-  // $('#itemCategory').show();
-  // $('#itemNameMens').hide();
-  // $('#itemNameWomens').hide();
-  // $('#itemNameAccessories').hide();
-  // $('#itemSize').hide();
-  // $('#itemQuantity').hide();
 
 $('#itemCategory').change(function() {
   if ($(this).val() === "mens") {
-    $('#itemNameMens').parent().removeClass('hide');
     $('#itemSize').parent().removeClass('hide');
-    $('#itemQuantity').parent().removeClass('hide');
   }
   if ($(this).val() === "womens") {
-      $('#itemNameWomens').parent().removeClass('hide');
       $('#itemSize').parent().removeClass('hide');
-      $('#itemQuantity').parent().removeClass('hide');
-  }
-  if ($(this).val() === "accessories") {
-      $('#itemNameAccessories').parent().removeClass('hide');
-      $('#itemNameMens').parent().addClass('hide');
-      $('#itemQuantity').parent().removeClass('hide');
   }
 });
-
 $('#itemCategory2').change(function() {
   if ($(this).val() === "mens") {
     $('#itemNameMens2').parent().removeClass('hide');
@@ -125,5 +111,142 @@ $('#itemCategory2').change(function() {
   if ($(this).val() === "accessories") {
       $('#itemNameAccessories2').parent().removeClass('hide');
       $('#itemQuantity2').parent().removeClass('hide');
+      $('#itemNameWomens2').parent().addClass('hide');
+      $('#itemNameMens2').parent().addClass('hide');
   }
 });
+
+$('#itemCategory3').change(function() {
+  if ($(this).val() === "mens") {
+    $('#itemNameMens3').parent().removeClass('hide');
+    $('#itemSize3').parent().removeClass('hide');
+    $('#itemQuantity3').parent().removeClass('hide');
+  }
+  if ($(this).val() === "womens") {
+      $('#itemNameWomens3').parent().removeClass('hide');
+      $('#itemSize3').parent().removeClass('hide');
+      $('#itemQuantity3').parent().removeClass('hide');
+  }
+  if ($(this).val() === "accessories") {
+      $('#itemNameAccessories3').parent().removeClass('hide');
+      $('#itemQuantity3').parent().removeClass('hide');
+  }
+});
+
+function sendPostReqest(event) {
+  event.preventDefault();
+
+  if ($('#itemCategory option:selected').val() === "mens" || "womens") {
+    createNewAppItem();
+  } if ($('#itemCategory option:selected').val() === "accessories") {
+    createNewAccItem();
+  }
+}
+
+function sendPutReqest(event) {
+  event.preventDefault();
+  if ($('#itemCategory option:selected').val() === "mens" || "womens") {
+    createNewAppItem();
+  } if ($('#itemCategory option:selected').val() === "accessories") {
+    createNewAccItem();
+  }
+}
+
+function sendDeleteReqest(event) {
+  event.preventDefault();
+  console.log("gone gone gone");
+}
+
+const categoryDelete = $('#itemCategory2 option:selected').val();
+const nameDelete = $('#itemName2').val();
+const sizeDelete = $('#itemSize2').val();
+const quantityDelete = $('#itemQuantity2').val();
+
+const categoryPut = $('#itemCategory').val();
+const namePut = $('#itemNameMens3').val();
+const sizePut = $('#itemSize3').val();
+const quantityPut = $('#itemQuantity3').val();
+
+const url = "http://localhost:8080/product"
+
+function createNewAppItem() {
+  const categoryPost = $('#itemCategory').val();
+  const namePost = $('#itemName').val();
+  const pricePost = $('#price').val();
+  const smallQuantityPost = $('#smallQuantity').val();
+  const mediumQuantityPost = $('#mediumQuantity').val();
+  const largeQuantityPost = $('#largeQuantity').val();
+  const xlQuantityPost = $('#xlQuantity').val();
+  const totalQuantityPost = Number(smallQuantityPost) + Number(mediumQuantityPost) + Number(largeQuantityPost)
+ + Number(xlQuantityPost);
+
+  let productPost = {
+    name: namePost,
+    category: categoryPost,
+    price: pricePost,
+    totalQuantity: totalQuantityPost,
+    smallQuantityAvailable: smallQuantityPost,
+    mediumQuantityAvailable: mediumQuantityPost,
+    largeQuantityAvailable: largeQuantityPost,
+    xlQuantityAvailable: xlQuantityPost
+  }
+  $.post(url, productPost)
+  .then(function(data) {
+     console.log(data)
+  })
+}
+
+function createNewAccItem() {
+const categoryPost = $('#itemCategory').val();
+const namePost = $('#itemName').val();
+const pricePost = $('#price').val();
+const totalQuantityPost = Number(smallQuantityPost) + Number(mediumQuantityPost) + Number(largeQuantityPost)
++ Number(xlQuantityPost);
+
+let productPost = {
+  name: namePost,
+  category: categoryPost,
+  price: pricePost,
+  totalQuantity: totalQuantityPost,
+}
+$.post(url, productPost)
+.then(function(data) {
+   console.log(data)
+   .then(function(response) {
+     getNewItem(url)
+   })
+})
+}
+
+function getNewItem(url) {
+  $.get(url).then(function(data){
+    data.json()
+.then(displayProduct)
+})
+}
+
+function displayProduct(product) {
+  for (var i=0; i < data.length; i++){
+    var newItem = data[i];
+
+    $('#inventoryTable').append(
+  `
+    <tr>
+      <td>${newItem.id}</td>
+      <td>${newItem.name}</td>
+      <td>${newItem.category}</td>
+      <td>${newItem.price}</td>
+      <td>${newItem.totalQuantity}</td>
+      <td>${newItem.smallQuantityAvailable}</td>
+      <td>${newItem.mediumQuantityAvailable}</td>
+      <td>${newItem.largeQuantityAvailable}</td>
+      <td>${newItem.xlQuantityAvailable}</td>
+    </tr>
+  `
+  );
+  }
+}
+
+function sendPutRequest() {
+
+}
